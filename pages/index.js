a110d66484307ac509527e3f74844762fe636add
Jack Herrington <jherr@pobox.com>
2022-02-26 06:04:55 -0800

    Switching to CSR

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,33 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export async function getServerSideProps(context) {
-  const pokemonResp = await fetch(
-    "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
-  );
-  const pokemon = await pokemonResp.json();
-  return {
-    props: {
-      pokemon,
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const pokemonResp = await fetch(
+//     "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
+//   );
+//   const pokemon = await pokemonResp.json();
+//   return {
+//     props: {
+//       pokemon,
+//     },
+//   };
+// }
 
-export default function Home({ pokemon }) {
+export default function Home({}) {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const pokemonResp = await fetch(
+        "https://raw.githubusercontent.com/jherr/pokemon/main/index.json"
+      );
+      setPokemon(await pokemonResp.json());
+    }
+    getPokemon();
+  });
+
   return (
     <div>
       <Head>
