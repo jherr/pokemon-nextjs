f3c2318313007f35fddc78f8f6657feb7e29947a
Jack Herrington <jherr@pobox.com>
2022-02-25 20:37:44 -0800

    12.1 revalidating

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,5 +1,14 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   console.log(">> modified");
   console.log(req.body?.head_commit?.modified);
-  res.status(200).json({ name: "John Doe" });
+
+  for (const file of req.body?.head_commit?.modified ?? []) {
+    if (file.includes("pokemon/")) {
+      const path = `${file}on`;
+      console.log(`Revalidating ${path}`);
+      await res.unstable_revalidate(`${path}`);
+    }
+  }
+
+  return res.json({ revalidated: true });
 }
--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -36,7 +36,7 @@ export async function getStaticProps({ params }) {
     props: {
       pokemon,
     },
-    revalidate: 60,
+    // revalidate: 60,
   };
 }
 
